f10235ed58bd242afdfa16926d9567753f87c61a
Rohit Khatkar <rohit.khatkar@ww.com>
2020-11-22 16:39:15 -0500

    JSP with Spring Boot.

---
+++ src/main/java/com/example/jspdemo/controller/web/JspController.java
@@ -0,0 +1,13 @@
+package com.example.jspdemo.controller.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class JspController {
+
+    @GetMapping("/")
+    public String jspDefault() {
+        return "index";
+    }
+}
